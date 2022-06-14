a2de95c81a70c8952affe557700149eb8ee36ca5
Jorge A. Benavides <jorge.benavides@wizeline.com>
2022-06-13 19:32:21 -0500

    roulette divisions with relative angle

--- RandomRoulette/CGPoint+Extensions.swift
+++ RandomRoulette/CGPoint+Extensions.swift
@@ -8,10 +8,16 @@
 import UIKit
 
 extension CGPoint {
+    /// displacement on `x` axis
     func dx(_ x: CGFloat) -> CGPoint {
         CGPoint(x: self.x + x, y: self.y)
     }
+    /// displacement on `y` axis
     func dy(_ y: CGFloat) -> CGPoint {
         CGPoint(x: self.x, y: self.y + y)
+    }
+    /// displacement on `x` and `y` axis
+    func displace(by point: CGPoint) -> CGPoint {
+        dx(point.x).dy(point.y)
     }
 }
--- RandomRoulette/Roulette.swift
+++ RandomRoulette/Roulette.swift
@@ -27,18 +27,16 @@ struct Roulette: View {
                 Path { path in
                     guard divisions > 1 else { return }
                     let center = CGPoint(x: geometry.size.width/2, y: geometry.size.height/2)
-                    let radius: CGFloat = min(geometry.size.width, geometry.size.height) / 2
+                    let radius: Double = min(geometry.size.width, geometry.size.height) / 2
                     var angle: Double = 360
                     let increment = angle / Double(divisions)
                     while angle > 0 {
-                        let offset: Double = 90 + increment/2
-                        let radian: Double = (angle - offset) * .pi / 180
-                        let x = radius * cos(radian)
-                        let y = radius * sin(radian)
-                        print(x, y)
+                        let offset: Double = angle - (90 + increment/2)
+                        let point = center.displace(by: self.point(for: offset, with: radius))
                         path.move(to: center)
-                        path.addLine(to: center.dy(y).dx(x))
-
+                        path.addLine(to: point)
+                        path.addRelativeArc(center: center, radius: radius, startAngle: Angle(degrees: offset), delta: Angle(degrees: increment), transform: .identity)
+                        path.addLine(to: center)
                         angle -= increment
                     }
                 }
@@ -46,10 +44,20 @@ struct Roulette: View {
             }
         }
     }
+
+    /// c^2 = a^2 + b^2
+    /// x = base, y = height
+    private func point(for angle: Double, with radius: Double) -> CGPoint {
+        let radian: Double = angle * .pi / 180
+        let x = radius * cos(radian)
+        let y = radius * sin(radian)
+        return CGPoint(x: x, y: y)
+    }
+
 }
 
 struct Roulette_Previews: PreviewProvider {
     static var previews: some View {
-        Roulette(divisions: 12)
+        Roulette(divisions: 12, color: .gray, divisionColor: .orange)
     }
 }
